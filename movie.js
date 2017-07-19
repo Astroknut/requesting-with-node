fc20dc797b6b56a06a6e8f8962f886959e852d18
Astroknut <charlesbarrett1521@gmail.com>
2017-07-19 16:50:03 -0600

    snippet v title

--- movie.js
+++ movie.js
@@ -7,9 +7,11 @@ const apiUrl = 'https://www.googleapis.com/customsearch/v1?' + apiKey + 'q=';
 function get(movieTitle) {
 	var URL = apiUrl + movieTitle + 'movie';
 	request(URL, function (error, response, body) {
-		console.log("Here's your snippet!- ", JSON.parse(body).items[0].title);
-		
-	});
+		//Const for 'body'
+		//Check 'body' for display link "www.imdb.com"
+		//If from imdb, then console.log
+		console.log("Here's your snippet!- ", JSON.parse(body).items[0].snippet);
+		});
 }
 
 module.exports = get
